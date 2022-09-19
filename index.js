e4179e9650d794c485e4e2b89f5f9d7d3a6f29a9
Theophilus Adesola <theophilusadessola@gmail.com>
2022-09-19 11:37:45 +0100

    Changed the syntax to ES6

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-function btnClick(press) {
+const btnClick = (press) => {
     if (expressionDisp.value == 0) {
         expressionDisp.value = press;
     } else if (expressionDisp.value != 0) {
         expressionDisp.value += press;
     }
-}
+};
 
-function calc() {
+const calc = () => {
     answerDisp.value = eval(expressionDisp.value);
     // expressionDisp.value = ""
-}
-function cls() {
+};
+const cls = () => {
     expressionDisp.value = "";
     answerDisp.value = "";
-}
-function backSpace() {
+};
+const backSpace = () => {
     expressionDisp.value = expressionDisp.value.slice(0, -1);
-}
+};
 
-function sfnClick() {
-    // expressionDisp.value += spress;
-    expressionDisp.value += btnClick("sin(");
-    answerDisp.value = sin(expressionDisp.value);
-}
+// const sfnClick = () => {
+//     // expressionDisp.value += spress;
+//     expressionDisp.value += btnClick("sin(");
+//     answerDisp.value = sin(expressionDisp.value);
+// };
